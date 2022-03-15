1e037c13cb9fd12e645bfcef44c1037df0c1ca3f
yongjieyeoh <divineknight86@gmail.com>
2022-03-16 00:03:53 +0800

    init commit

---
+++ workshop01/routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  var textArray = [
+      'Logic will get you from A to B. Imagination will take you everywhere.',
+      'There are 10 kinds of people. Those who know binary and those who don\'t.',
+      'There are two ways of constructing a software design. One way is to make it so simple that there are obviously no deficiencies and the other is to make it so complicated that there are no obvious deficiencies.',
+      'It\'s not that I\'m so smart, it\'s just that I stay with problems longer.',
+      'It is pitch dark. You are likely to be eaten by a grue.'
+  ];
+
+  var randomNumber = Math.floor(Math.random()*textArray.length);
+
+  res.render('index', { title: textArray[randomNumber] });
+});
+
+module.exports = router;
